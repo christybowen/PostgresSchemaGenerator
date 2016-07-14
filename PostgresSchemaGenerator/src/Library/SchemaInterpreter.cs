a69022f022a4e6735eb114c71f0f2cba5302bed4
Jason H Bennett <jasonb@squarehook.com>
2016-07-14 14:47:40 -0600

    Added comments to the schema interpreter

--- PostgresSchemaGenerator/src/Library/SchemaInterpreter.cs
+++ PostgresSchemaGenerator/src/Library/SchemaInterpreter.cs
@@ -7,15 +7,39 @@ using Npgsql;
 
 namespace PostgresSchemaGenerator.src.Library
 {
+    /// <summary>
+    /// This class handles the interpretation of the schema for a given table.
+    /// </summary>
     class SchemaInterpreter
     {
+        /// <summary>
+        /// The handle to the PostGres connection.
+        /// </summary>
         private NpgsqlCommand sqlHandle;
 
+        /// <summary>
+        /// The name of the table to get the schema for.
+        /// </summary>
+        private String tableName = null;
+
+        /// <summary>
+        /// The iteratable list of strings that make up the c sharp class.
+        /// </summary>
+        private List<String> cFile;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="cmd">The connection handle.</param>
         public SchemaInterpreter(NpgsqlCommand cmd)
         {
             this.sqlHandle = cmd;
         }
 
+        /// <summary>
+        /// Pulls the information about the schema from the database for the specified table/view.
+        /// </summary>
+        /// <param name="viewName">The table or view to pull information on.</param>
         public void pullSchema(String viewName)
         {
             List<List<String>> output = new List<List<String>>();
@@ -46,7 +70,23 @@ namespace PostgresSchemaGenerator.src.Library
                 Console.WriteLine(e.Message);
             }
 
+            this.tableName = viewName;
+
             // Do what you need to with the schema that was pulled back.
+        }
+
+        /// <summary>
+        /// Writes the Schema currently stored by the object to a file.
+        /// </summary>
+        /// <param name="location">The file location.</param>
+        public void writeSchema(String location)
+        {
+            // TODO: Need to make a sanity check for if the file exists and delete it if it does.
+
+            if (this.tableName != null)
+            {
+                System.IO.File.WriteAllLines(@location + this.tableName, this.cFile);
+            }
         }
     }
 }
